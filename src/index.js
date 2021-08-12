893f27e615acd5178da6d606a859e2fed757a864
irakoze bruce <irabruce20@gmail.com>
2021-08-12 14:52:06 +0200

    final Modifications

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import TodoContainer from "./functionBased/components/TodoContainer"
+import TodoContainer from "./classBased/components/TodoContainer"
 import './functionBased/App.css'
 
 ReactDOM.render(
